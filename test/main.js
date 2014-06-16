b61c25e743b311be6e988dd69b436b3b8a788750
Contra <contra@maricopa.edu>
2014-06-16 12:29:06 -0700

    more fakes

--- test/main.js
+++ test/main.js
@@ -14,14 +14,45 @@ after(function(){
 });
 
 describe('Customer', function() {
-  it('should be able to construct from apartment number', function(done){
+  it('should be able to construct with address', function(done){
+    var user = pjs.customer({
+      address: '1234 Fake St'
+    });
+    should.exist(user.location);
+    user.location.address.should.equal('1234 Fake St');
+    user.location.secondaryAddress.should.eql(['NON', '-']);
+    done();
+  });
+
+  // secondary address crap
+  it('should be able to construct with apartment number', function(done){
     var user = pjs.customer({
       address: '1234 Fake St',
       apt: 2315
     });
     should.exist(user.location);
     user.location.address.should.equal('1234 Fake St');
-    user.location.secondaryAddress.should.eql(['APT', '2315']);
+    user.location.secondaryAddress.should.eql(['APT', 2315]);
+    done();
+  });
+  it('should be able to construct with suite number', function(done){
+    var user = pjs.customer({
+      address: '1234 Fake St',
+      suite: 2315
+    });
+    should.exist(user.location);
+    user.location.address.should.equal('1234 Fake St');
+    user.location.secondaryAddress.should.eql(['SUITE', 2315]);
+    done();
+  });
+  it('should be able to construct with floor number', function(done){
+    var user = pjs.customer({
+      address: '1234 Fake St',
+      floor: 2
+    });
+    should.exist(user.location);
+    user.location.address.should.equal('1234 Fake St');
+    user.location.secondaryAddress.should.eql(['FLOOR', 2]);
     done();
   });
 });
